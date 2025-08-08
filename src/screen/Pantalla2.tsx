ec20ab190bc6e9df3682554eb581d6b735a4f345
bryxngp <bryxngp@gmail.com>
2025-08-07 19:55:28 -0500

    Bryan

--- src/screen/Pantalla2.tsx
+++ src/screen/Pantalla2.tsx
@@ -21,19 +21,15 @@ export const Pantalla2 = ({ navigation }: Props) => {
 
     const changeForm = (property: string, value: string): void => {
         //console.log(property + ':' + value)
-        setNumero({ ...formNumero, [property]: parseFloat(value) || 0 }); //sacar copia ... para acceder a la informacion
+        setNumero({ ...formNumero, [property]: parseFloat(value) }); //sacar copia ... para acceder a la informacion
     }
 
-    const dividir = (): void => {
-        console.log(formNumero);
-    }
 
     const filtrarSoloNumeros = (texto: string): string => texto.replace(/[^0-9.]/g, '');
 
     const division = () => {
         const num1 = formNumero.numero1;
         const num2 = formNumero.numero2;
-
         if (num1 === 0 && num2 === 0) {
             alert('Indeterminacion');
             return;
@@ -50,9 +46,7 @@ export const Pantalla2 = ({ navigation }: Props) => {
 
 
     return (
-
         <View style={styles.container2}>
-
             <SafeAreaView style={styles.container2} edges={['left', 'right']}>
                 <View style={styles.botonAtras}>
                     <Text style={styles.atras} onPress={() => navigation.navigate('Pantalla1')}>◄</Text>
@@ -81,15 +75,8 @@ export const Pantalla2 = ({ navigation }: Props) => {
                     <TouchableOpacity style={styles.superButton2} onPress={division}>
                         <Text style={styles.dividir}>Dividir</Text>
                     </TouchableOpacity>
-
-
-
-
                 </SafeAreaView>
-
             </SafeAreaView>
-
         </View>
-
     )
 }
